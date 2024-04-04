b678a0ff1c5ca0586d7e30ae4f58a826e3bc1a84
utkarshagrawal <utkarshagrawal09jan@gmail.com>
2024-04-05 00:56:02 +0530

    Fixed bug

--- backend/middlewares/authentication.js
+++ backend/middlewares/authentication.js
@@ -5,7 +5,7 @@ const authenticated = async (req, res, next) => {
     const token = req.headers.Authorization;
     if (token) {
         try {
-            const payload = jwt.verify(signature, process.env.APP_SECRET_KEY);
+            const payload = jwt.verify(signature, process.env.SECRET_KEY);
             next();
         } catch (error) {
             return res.json({ error: error.message });
--- frontend/components/User/Landing/LandingPage.jsx
+++ frontend/components/User/Landing/LandingPage.jsx
@@ -9,12 +9,14 @@ export default function LandingPage() {
 
     return (
         <div className="bg-gradient-to-br from-blue-200 to-blue-400 h-screen flex items-center justify-center relative">
-            <Link to="/admin/signin" className="text-white font-semibold text-lg absolute top-10 right-10 hover:underline flex items-center gap-2">
-                Sign in as admin
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
-                </svg>
-            </Link>
+            {!localStorage.getItem('userId') && (
+                <Link to="/admin/signin" className="text-white font-semibold text-lg absolute top-10 right-10 hover:underline flex items-center gap-2">
+                    Sign in as admin
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+                    </svg>
+                </Link>
+            )}
             {localStorage.getItem('userId') ? <ChatWindow name={name} /> : <StartChat name={name} setName={setName} />}
         </div>
     )
